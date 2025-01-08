a5f210b9f2ade0aaf09faf8193d00bb3a0752395
Ben Cooley <benjcooley@gmail.com>
2025-01-08 02:24:49 -0800

    feat: Add demo mode and room arrangement options with improved map generation

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ import math
 import skia
 
 from graphics.conversions import grid_to_drawing, grid_to_drawing_size
+from map.mapelement import MapElement
 from map.room import Room
 from map.map import Map
 from map.door import Door, DoorOrientation
 from map.passage import Passage
 from algorithms.rotation import Rotation
 from map.props import ColumnType, Altar, Coffin, Dais, Rock
-from map.arrange import PropType, arrange_columns, ColumnArrangement, arrange_random_props, arrange_prop
+from map.arrange import PropType, arrange_columns, ColumnArrangement, \
+    arrange_random_props, arrange_rooms, ArrangeRoomStyle
 from options import Options
+from typing import List
 
 def main():
     options = Options()
@@ -23,42 +26,69 @@ def main():
     
     # Create map
     dungeon_map = Map(options)
+
+    DEMO = False
+
+    if DEMO:
+
+        rooms: List[MapElement] = []
+
+        # Add central rectangular room (5x5, centered at 0,0)
+        # Since we want grid alignment and center at 0,0, we'll offset by -2,-2
+        start_room0 = dungeon_map.add_rectangular_room(-10, -2, 5, 5) 
+        rooms.append(start_room0)   
+        
+        # Add 5-grid long passage
+        passage0 = Passage.from_grid(-5, 0, 5, 1, dungeon_map)
+        rooms.append(passage0)
+        dungeon_map.add_element(passage0)
+
+        # Add central rectangular room (5x5, centered at 0,0)
+        # Since we want grid alignment and center at 0,0, we'll offset by -2,-2
+        start_room = dungeon_map.add_rectangular_room(-2, -2, 5, 5)
+        rooms.append(start_room)
+        
+        # Add door to the right of the room (at x=3, centered vertically)
+        first_door = Door.from_grid(3, 0, DoorOrientation.HORIZONTAL, dungeon_map, open=True)
+        rooms.append(first_door)
+        dungeon_map.add_element(first_door)
+        
+        # Add 5-grid long passage
+        passage = Passage.from_grid(4, 0, 5, 1, dungeon_map)
+        rooms.append(passage)
+        dungeon_map.add_element(passage)
+        
+        # Add closed door at the end of the passage
+        second_door = Door.from_grid(9, 0, DoorOrientation.HORIZONTAL, dungeon_map, open=False)
+        rooms.append(second_door)
+        dungeon_map.add_element(second_door)
+        
+        # Add circular room (5x5 grid units, 11 units right of start room)
+        end_room = dungeon_map.add_circular_room(10, -2, 5)
+        rooms.append(end_room)
+        
+        # Connect everything together
+        start_room0.connect_to(passage0)
+        passage0.connect_to(start_room)
+        start_room.connect_to(first_door)
+        first_door.connect_to(passage)
+        passage.connect_to(second_door)
+        second_door.connect_to(end_room)
+        
+        # Add props to rooms and passage
+        # Test vertical row layout for columns
+        arrange_columns(start_room, ColumnArrangement.VERTICAL_ROWS, column_type=ColumnType.SQUARE)
+        
+        # Add square columns in a circle arrangement
+        arrange_columns(end_room, ColumnArrangement.CIRCLE, column_type=ColumnType.SQUARE)
+        
+        # Add some rocks
+        for room in rooms:
+            arrange_random_props(room, [PropType.SMALL_ROCK], min_count=0, max_count=5)
+            arrange_random_props(room, [PropType.MEDIUM_ROCK], min_count=0, max_count=5)
     
-    # Add central rectangular room (5x5, centered at 0,0)
-    # Since we want grid alignment and center at 0,0, we'll offset by -2,-2
-    start_room = dungeon_map.add_rectangular_room(-2, -2, 5, 5)
-    
-    # Add door to the right of the room (at x=3, centered vertically)
-    first_door = Door.from_grid(3, 0, DoorOrientation.HORIZONTAL, dungeon_map, open=True)
-    dungeon_map.add_element(first_door)
-    
-    # Add 5-grid long passage
-    passage = Passage.from_grid(4, 0, 5, 1, dungeon_map)
-    dungeon_map.add_element(passage)
-    
-    # Add closed door at the end of the passage
-    second_door = Door.from_grid(9, 0, DoorOrientation.HORIZONTAL, dungeon_map, open=False)
-    dungeon_map.add_element(second_door)
-    
-    # Add circular room (5x5 grid units, 11 units right of start room)
-    end_room = dungeon_map.add_circular_room(10, -2, 5)
-    
-    # Connect everything together
-    start_room.connect_to(first_door)
-    first_door.connect_to(passage)
-    passage.connect_to(second_door)
-    second_door.connect_to(end_room)
-    
-    # Add props to rooms and passage
-    # Test vertical row layout for columns
-    arrange_columns(start_room, ColumnArrangement.VERTICAL_ROWS, column_type=ColumnType.SQUARE)
-    
-    # Add square columns in a circle arrangement
-    arrange_columns(end_room, ColumnArrangement.CIRCLE, column_type=ColumnType.SQUARE)
-    
-    # Add some rocks
-    arrange_random_props(end_room, [PropType.MEDIUM_ROCK], min_count=0, max_count=2)
-    arrange_random_props(end_room, [PropType.SMALL_ROCK], min_count=0, max_count=2)
+    else:
+        arrange_rooms(dungeon_map, ArrangeRoomStyle.LINEAR, min_rooms=2, max_rooms=2, min_size=3, max_size=5)
 
     # Draw the map (which will draw all rooms)
     dungeon_map.render(canvas)
